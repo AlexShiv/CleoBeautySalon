90d82dd13be794ecc12fce91ecc141d817b65925
Aleksey <Shiyanov1998@mail.ru>
2019-12-20 01:46:43 +0400

    Добавление тестовых Post запросов

--- src/main/java/ru/asu/cleo/controller/HelloWorldController.java
+++ src/main/java/ru/asu/cleo/controller/HelloWorldController.java
@@ -1,9 +1,9 @@
 package ru.asu.cleo.controller;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.asu.cleo.domain.Service;
 import ru.asu.cleo.repository.ServiceRepository;
 import ru.asu.cleo.service.dto.MainResponse;
@@ -49,9 +49,30 @@ public class HelloWorldController {
         return ResponseEntity.ok(employee);
     }
 
-    private class Test {
+    @PostMapping
+    private ResponseEntity sendTestData(@RequestBody Test test) {
+        if (test.duration<5) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PostMapping("/listObject")
+    private ResponseEntity sendTestData(@RequestBody List<Test> test) {
+        for (Test test1 : test) {
+            if (test1.duration>=5) {
+                return ResponseEntity.badRequest().build();
+            }
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    private static class Test {
+        @JsonProperty("id")
         private Long id;
+        @JsonProperty("name")
         private String name;
+        @JsonProperty("duration")
         private Integer duration;
 
         public Long getId() {
