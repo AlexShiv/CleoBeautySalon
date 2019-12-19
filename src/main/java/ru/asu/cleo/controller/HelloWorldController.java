ebc606d4226a769bff8cc2527b4e5f943c9f7956
Aleksey <Shiyanov1998@mail.ru>
2019-12-20 00:00:51 +0400

    Добавление дополнительных тестовых контроллеров

--- src/main/java/ru/asu/cleo/controller/HelloWorldController.java
+++ src/main/java/ru/asu/cleo/controller/HelloWorldController.java
@@ -4,14 +4,45 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.asu.cleo.service.dto.MainResponseDTO;
+import ru.asu.cleo.domain.Client;
+import ru.asu.cleo.domain.Employee;
+import ru.asu.cleo.repository.EmployeeRepository;
+import ru.asu.cleo.service.dto.MainListResponse;
+import ru.asu.cleo.service.dto.MainResponse;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/helloWorld")
 public class HelloWorldController {
 
-    @GetMapping("/helloWorld")
-    private ResponseEntity<MainResponseDTO<String>> getHelloWorld() {
-        return ResponseEntity.ok(new MainResponseDTO<>("Leonid - Papyrus"));
+    private final EmployeeRepository employeeRepository;
+
+    public HelloWorldController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+
+    @GetMapping("")
+    private ResponseEntity<MainResponse<String>> getHelloWorld() {
+        return ResponseEntity.ok(new MainResponse<>("Leonid - Papyrus"));
+    }
+
+    @GetMapping("/list")
+    private ResponseEntity<MainListResponse<String>> getHelloWorldList() {
+        List<String> strings = Arrays.asList("Lenya", "Nata", "Tanya", "Lesha");
+        return ResponseEntity.ok(new MainListResponse<>(strings));
+    }
+
+    @GetMapping("/listObject")
+    private ResponseEntity<MainListResponse<Employee>> getHelloWorldListObject() {
+        return ResponseEntity.ok(new MainListResponse<>(employeeRepository.findAll()));
+    }
+
+    @GetMapping("/object")
+    private ResponseEntity<MainResponse<Employee>> getHelloWorldObject() {
+        Employee employee = employeeRepository.findById(1L).orElseThrow();
+        return ResponseEntity.ok(new MainResponse<>(employee));
     }
 }
